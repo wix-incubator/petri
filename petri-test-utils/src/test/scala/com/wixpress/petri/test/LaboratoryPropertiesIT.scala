5a4ec2cba2a010975596bf24f7f599ccbe3faec6
Sagi Bernstein <sagib@wix.com>
2016-07-11 11:07:37 +0300

    moved the test to test-utils and refactored

--- petri-test-utils/src/test/scala/com/wixpress/petri/test/LaboratoryPropertiesIT.scala
+++ petri-test-utils/src/test/scala/com/wixpress/petri/test/LaboratoryPropertiesIT.scala
@@ -3,13 +3,13 @@ package com.wixpress.petri.test
 import com.wixpress.petri.test.SampleAppRunner.DEFAULT_PATH_TO_WEBAPP
 import org.specs2.mutable.SpecificationWithJUnit
 
+import scala.io.Source.fromFile
+
 class LaboratoryPropertiesIT extends SpecificationWithJUnit {
 
   "laboratory.properties file" should {
-    "be resilient to IT tests (remain the same as before instantiating SampleAppRunner)" in {
+    "remain the same as before instantiating SampleAppRunner" in {
       val reporterIntervalDifferentVal = 7
-      def labPropFileContent = scala.io.Source.fromFile(DEFAULT_PATH_TO_WEBAPP
-        + "/WEB-INF/laboratory.properties").mkString
 
       val originalLabPropsContent = labPropFileContent
       val sampleApp = new SampleAppRunner(9015, DEFAULT_PATH_TO_WEBAPP, reporterIntervalDifferentVal, false)
@@ -20,4 +20,6 @@ class LaboratoryPropertiesIT extends SpecificationWithJUnit {
       labPropFileContent mustEqual originalLabPropsContent
     }
   }
+
+  def labPropFileContent = fromFile(s"$DEFAULT_PATH_TO_WEBAPP/WEB-INF/laboratory.properties").mkString
 }
