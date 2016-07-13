77fa1e53e0605ca8d837dbfbc152c7e2f6dac728
Sagi Bernstein <sagib@wix.com>
2016-07-13 16:01:14 +0300

    resolver test refactoring

--- laboratory-servlet-api-integration/src/test/java/com/wixpress/petri/laboratory/LanguageResolverTest.scala
+++ laboratory-servlet-api-integration/src/test/java/com/wixpress/petri/laboratory/LanguageResolverTest.scala
@@ -16,32 +16,5 @@ class LanguageResolverTest extends SpecificationWithJUnit {
       val config = FilterParametersExtractorsConfig()
       resolver.resolve(request, config) must beEqualTo (request.getLocale.getLanguage)
     }
-
-    "resolve by header 'SOME_HEADER' when configured" in new Context {
-      val config = FilterParametersExtractorsConfig(Map("Language" -> List(("Header", "SOME_HEADER"))))
-      val someLang = "he"
-      request.addHeader("SOME_HEADER", someLang)
-      resolver.resolve(request, config) must beEqualTo (someLang)
-    }
-
-    "resolve by param 'SOME_PARAM' when configuration before that are not present" in new Context {
-      val config = FilterParametersExtractorsConfig(
-        Map("Language" ->
-          List(("Header", "SOME_HEADER"),
-            ("Cookie", "SOME_COOKIE"),
-            ("Param", "SOME_PARAM"))))
-      val someLang = "he"
-      request.addParameter("SOME_PARAM", someLang)
-      resolver.resolve(request, config) must beEqualTo (someLang)
-    }
-
-    "resolve by default behaviour when configuration before that are not present" in new Context {
-      val config = FilterParametersExtractorsConfig(
-        Map("Language" ->
-          List(("Header", "SOME_HEADER"),
-            ("Cookie", "SOME_COOKIE"),
-            ("Param", "SOME_PARAM"))))
-      resolver.resolve(request, config) must beEqualTo (request.getLocale.getLanguage)
-    }
   }
 }
