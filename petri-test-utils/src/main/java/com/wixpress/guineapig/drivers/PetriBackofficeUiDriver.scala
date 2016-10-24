dd6c0cb241568e5bc743d6423c9981d618a267eb
talyag <talyag@wix.com>
2016-10-24 21:52:06 +0300

    greatly reduce logs of runnable server tests + gp server shouldn't ship with DEBUG level

--- petri-test-utils/src/main/java/com/wixpress/guineapig/drivers/PetriBackofficeUiDriver.scala
+++ petri-test-utils/src/main/java/com/wixpress/guineapig/drivers/PetriBackofficeUiDriver.scala
@@ -1,16 +1,25 @@
 package com.wixpress.guineapig.drivers
 
+import com.gargoylesoftware.htmlunit.{SilentCssErrorHandler, WebClient}
 import org.openqa.selenium.By
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
 
 
 class PetriBackofficeUiDriver(port: Int) extends org.specs2.mutable.Specification{
-  private val driver = new HtmlUnitDriver(true)
+  private val driver = new NoCssErrorsHtmlUnitDriver()
   private val pageUrl = s"http://localhost:$port/v1/index#/login/home/active"
 
   def numberOfActiveExperimentsIs(num: Int) = {
     driver.get(pageUrl)
     driver.findElement(By.id("mainView")).getText must eventually(contain(s"active Experiments ($num)"))
+  }
+
+  class NoCssErrorsHtmlUnitDriver extends HtmlUnitDriver(true){
+
+    override def modifyWebClient(client: WebClient): WebClient = {
+      client.setCssErrorHandler(new SilentCssErrorHandler)
+      client
+    }
   }
 
 }
