6c40752fd631db1587d2290f0e9ce81fa929e60d
slavan <slavan@wix.com>
2016-12-27 22:43:38 +0200

    improve FilterParametersExtractorsConfigTest (matchers+ readability)

--- laboratory-servlet-api-integration/src/test/java/com/wixpress/petri/laboratory/FilterParametersExtractorsConfigTest.scala
+++ laboratory-servlet-api-integration/src/test/java/com/wixpress/petri/laboratory/FilterParametersExtractorsConfigTest.scala
@@ -1,7 +1,11 @@
 package com.wixpress.petri.laboratory
 
+import org.specs2.matcher.Matcher
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.specification.Scope
+import com.wixpress.petri.laboratory.FilterParameters._
+import com.wixpress.petri.laboratory.FilterParametersConfigOptions.Converter
+import scala.reflect.ClassTag
 
 /**
  * Created by litalt on 27/12/16.
@@ -16,30 +20,36 @@ class CustomConverter2 extends Converter[String] {
 
 class FilterParametersExtractorsConfigTest extends SpecificationWithJUnit {
 
-  import com.wixpress.petri.laboratory.FilterParametersExtractorsConfig._
-  trait Context extends Scope {
-    val someConverter1 = classOf[CustomConverter1].getName
-    val someConverter2 = classOf[CustomConverter2].getName
-  }
+  import FilterParametersExtractorsConfig._
 
   "instantiate converters" should {
     "instantiate custom converter properly according to config" in new Context {
       val config = new FilterParametersExtractorsConfig(Map(
-        FilterParameters.Language.toString -> List((FilterParametersConfigOptions.Converter.toString, someConverter1))))
-      instantiateConverters(config).converters.get(FilterParameters.Language.toString).get must beAnInstanceOf[CustomConverter1]
+        Language.toString -> List((Converter.toString, someConverter1))))
+      instantiateConverters(config).converters must haveConverterOf[CustomConverter1](Language.toString)
     }
     
     "instantiate custom converters per each of filter parameters" in new Context {
       val config = new FilterParametersExtractorsConfig(Map(
-        FilterParameters.Language.toString -> List((FilterParametersConfigOptions.Converter.toString, someConverter1)),
-        FilterParameters.Country.toString -> List((FilterParametersConfigOptions.Converter.toString, someConverter1)),
-        FilterParameters.UserId.toString -> List((FilterParametersConfigOptions.Converter.toString, someConverter2))))
+        Language.toString -> List((Converter.toString, someConverter1)),
+        Country.toString -> List((Converter.toString, someConverter1)),
+        UserId.toString -> List((Converter.toString, someConverter2))))
 
       val converters = instantiateConverters(config).converters
+
       converters.size must_== 3
-      converters.get(FilterParameters.Language.toString).get must beAnInstanceOf[CustomConverter1]
-      converters.get(FilterParameters.Country.toString).get must beAnInstanceOf[CustomConverter1]
-      converters.get(FilterParameters.UserId.toString).get must beAnInstanceOf[CustomConverter2]
+      converters must haveConverterOf[CustomConverter1](Language.toString)
+      converters must haveConverterOf[CustomConverter1](Country.toString)
+      converters must haveConverterOf[CustomConverter2](UserId.toString)
+    }
+  }
+
+  trait Context extends Scope {
+    val someConverter1 = classOf[CustomConverter1].getName
+    val someConverter2 = classOf[CustomConverter2].getName
+
+    def haveConverterOf[T <: Converter[_]: ClassTag](key: String): Matcher[Map[String, Converter[_]]] = {
+      beSome(beAnInstanceOf[T]) ^^ { (_: Map[String, Converter[_]]).get(key) aka "converter" }
     }
   }
 }
